69d97916615ceb2a0d988cab7cc2c7604afac3c8
s2angji <83641239+s2angji@users.noreply.github.com>
2022-06-28 17:13:00 +0900

    20220628 Object Detection 2 Stage 모델 실습

---
+++ 6월/5주차/0628 [심화] Object Detection 2 Stage 모델 실습/실습/aug_test.py
@@ -0,0 +1,136 @@
+""" bbox aug 적용되는 지 확인 코드"""
+from PIL import Image
+import cv2
+import numpy as np
+import time
+import torch
+import torchvision
+from torch.utils.data import Dataset
+from torchvision import transforms
+from matplotlib import pyplot as plt
+import os
+import random
+import data_exploration as data_ex
+import albumentations as albumentations
+from albumentations.pytorch import ToTensorV2
+
+
+class TorchvisionDataset(Dataset):
+    def __init__(self, path, transform=None):
+        self.path = path
+        self.imgs = list(sorted(os.listdir(self.path)))
+        self.transform = transform
+
+    def __getitem__(self, index):
+        file_image = self.imgs[index]
+        file_label = self.imgs[index][:-3] + 'xml'
+        img_path = os.path.join(self.path, file_image)
+
+        if 'test' in self.path:
+            label_path = os.path.join("test_annotations/", file_label)
+        else:
+            label_path = os.path.join("annotations/", file_label)
+
+        img = Image.open(img_path).convert("RGB")
+
+        target = data_ex.generate_target(label_path)
+
+        start_t = time.time()
+        if self.transform:
+            img = self.transform(img)
+
+        total_time = (time.time() - start_t)
+
+        return img, target, total_time
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+class AlbumentationsDataset(Dataset):
+    def __init__(self, path, transform=None):
+        self.path = path
+        self.imgs = list(sorted(os.listdir(self.path)))
+        self.transform = transform
+
+    def __getitem__(self, index):
+        file_image = self.imgs[index]
+        file_label = self.imgs[index][:-3] + 'xml'
+        img_path = os.path.join(self.path, file_image)
+
+        if 'test' in self.path:
+            label_path = os.path.join("test_annotations/", file_label)
+        else:
+            label_path = os.path.join("annotations/", file_label)
+
+        """Read an images with OpenCV"""
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        target = data_ex.generate_target(label_path)
+
+        start_t = time.time()
+        # if self.transform:
+        #     img = self.transform(img)
+        to_tensor = torchvision.transforms.ToTensor()
+
+        if self.transform:
+            transformed = self.transform(
+                image=image, bboxes=target['boxes'], labels=target['labels'])
+            image = transformed['image']
+            target = {'boxes': transformed['bboxes'],
+                      'labels': transformed['labels']}
+
+        total_time = (time.time() - start_t)
+
+        # image = to_tensor(img)
+
+        return image, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+torchvision_transform = transforms.Compose([
+    transforms.Resize((300, 300)),
+    transforms.RandomCrop(224),
+    transforms.ColorJitter(brightness=0.15, contrast=0.15,
+                           saturation=0.2, hue=0.18),
+    transforms.RandomHorizontalFlip(p=0.8),
+    transforms.ToTensor()
+])
+
+bbox_transform = albumentations.Compose([
+    albumentations.HorizontalFlip(p=1),
+    albumentations.Rotate(p=1),
+    ToTensorV2(),
+], bbox_params=albumentations.BboxParams(
+    format='pascal_voc', label_fields=['labels']))
+
+bbox_transform_dataset = AlbumentationsDataset(
+    path="./images/", transform=bbox_transform
+)
+
+torchvision_dataset = TorchvisionDataset(
+    path="./images/", transform=torchvision_transform)
+
+
+only_totensor = transforms.ToTensor()
+
+torchvision_dataset_no_transform = TorchvisionDataset(
+    path="./images/", transform=only_totensor)
+
+img, annot, transform_time = torchvision_dataset_no_transform[0]
+
+print("transform 적용 전 ")
+data_ex.plot_image(img, annot)
+
+img, annot, transform_time = torchvision_dataset[0]
+print("transform 적용 후 ")
+data_ex.plot_image(img, annot)
+
+img, annot = bbox_transform_dataset[0]
+print(img)
+
+
+print("bbox 적용")
+data_ex.plot_image(img, annot)
